684edfc971e73beb57174fa551abe72801bea5f3
naripok <fernando.canteruccio@gmail.com>
2024-05-30 20:10:46 -0300

    add workflows and api example script

---
+++ Makefile
@@ -0,0 +1,2 @@
+up:
+	python main.py --listen 0.0.0.0 --port 8080 --preview-method auto
---
+++ api/main.py
@@ -0,0 +1,28 @@
+import json
+from urllib import request
+
+SERVER_URL = "http://192.168.18.9:8080/prompt"
+
+
+def get_node_by_title(workflow, title):
+    for node in workflow.values():
+        if node["_meta"]["title"] == title:
+            return node
+
+
+def queue_prompt(workflow):
+    p = {"prompt": workflow}
+    data = json.dumps(p).encode("utf-8")
+    req = request.Request(SERVER_URL, data=data)
+    return request.urlopen(req).read()
+
+
+if __name__ == "__main__":
+    prompt_workflow = json.load(open("api/workflows/workflow_api.json"))
+
+    positive_prompt = get_node_by_title(prompt_workflow, "Positive Prompt")
+    if not positive_prompt:
+        raise ValueError("Positive Prompt not found")
+
+    positive_prompt["inputs"]["text"] = "a bottle full of wonders"
+    print(queue_prompt(prompt_workflow))
